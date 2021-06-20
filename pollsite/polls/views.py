c2cb04e25742a16da4b29b783c5aec7402ac96e2
Nut Milk <85531672+big-cock-benny@users.noreply.github.com>
2021-06-21 00:21:49 +0100

    created more views and linked up the URLs

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -7,3 +7,13 @@ from django.shortcuts import render
 # HTTP response to the function called by the path function in urls
 def index(request) -> HttpResponse:
     return HttpResponse("Hello, world")
+
+def detail(request, question_id) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id) -> HttpResponse:
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}")
