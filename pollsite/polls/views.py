3a51576d3cd940909fe8ae5658e1f2e41e1240d4
Nut Milk <85531672+big-cock-benny@users.noreply.github.com>
2021-06-21 14:17:15 +0100

    Created templates

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -7,10 +7,12 @@ from .models import Question
 # Here we create the simplest view possible by just returning the
 # HTTP response to the function called by the path function in urls
 def index(request) -> HttpResponse:
+
     # grabbing the 5 most recent polls from our Questions model
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    
+    return render(request, 'polls/index.html', context=context)
 
 def detail(request, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're looking at question {question_id}.")
