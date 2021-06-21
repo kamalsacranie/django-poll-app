c9f0f143f9e35a87119d9bf17bf544392549ee49
Nut Milk <85531672+big-cock-benny@users.noreply.github.com>
2021-06-21 16:09:01 +0100

    Finished the forms stuff and we redirect to a results page

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
-from .models import Question
+from django.http import (
+    HttpResponse, 
+    Http404, 
+    HttpResponseRedirect,
+)
+
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -22,9 +27,24 @@ def detail(request, question_id: int) -> HttpResponse:
         raise Http404("Question does not exist")
     return render (request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id: int) -> HttpResponse:
-    response = f"You're looking at the results of question {question_id}"
-    return HttpResponse(response)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id: int) -> HttpResponse:
-    return HttpResponse(f"You're voting on question {question_id}")
+    # Getting the relevant object using the get or 404 shortcut
+    question = get_object_or_404(Question, pk=question_id)
+
+    # Using the POST data from form and trying to assign it to the choice
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # rendering the form again
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
